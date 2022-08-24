6eaed5095869620f65adb81de33968e3f0ad7919
Bünyamin Mete <108274379+BunyaminMete@users.noreply.github.com>
2022-08-24 16:30:23 +0300

    Delete example directory

--- example/src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-import { Deneme } from 'hubstudio-highlight'
-const App = () => {
-  return <Deneme text="Create React Library Example 😄" search='creat' />
-}
-
-export default App
