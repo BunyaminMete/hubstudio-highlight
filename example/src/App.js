8ca2cbe07f232888031148e1d72992134fdfd159
Bunyamin Mete <kaanmetee5@gmail.com>
2022-08-24 14:00:43 +0300

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'hubstudio-highlight'
+import 'hubstudio-highlight/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
